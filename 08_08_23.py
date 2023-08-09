fefb15da16d56c24709aa3f4c6af35c0d64a270c
edeoz64 <u20211196484@usco.edu.co>
2023-08-08 19:06:30 -0500

    actividad uno completada

--- 08_08_23.py
+++ 08_08_23.py
@@ -1,1 +1,19 @@
-a = 34
+a = int(input('Ingrese el primer número :'))
+b = int(input('Ingrese el segundo número :'))
+
+suma = a + b
+resta = a - b
+mult = a*b
+div = a/b
+res = a % b
+
+print('---------------------------------------')
+print('El resultado de la suma es :'+ str(suma))
+print('---------------------------------------')
+print('El resultado de la resta es :'+ str(resta))
+print('---------------------------------------')
+print('El resultado de la multiplicación es :'+ str(mult))
+print('---------------------------------------')
+print('El resultado de la división es :'+ str(div))
+print('---------------------------------------')
+print('El residuo de la división es :'+ str(res))
