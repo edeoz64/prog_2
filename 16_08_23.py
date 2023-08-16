9388fbca96e25d33ae1e91eacee7d25ef2e46cb3
edeoz64 <u20211196484@usco.edu.co>
2023-08-16 18:57:36 -0500

    clase_16_08_23

---
+++ 16_08_23.py
@@ -0,0 +1,66 @@
+#listas o arreglos
+import os
+os.system ('cls')
+
+'''
+varlist = ['Duvan', True, 5.5, 9]
+
+print(varlist[0])
+print(varlist[1])
+print(varlist[2])
+print(varlist[3])
+
+
+#del varlist[2] #elimina elementos dentro de la lista
+
+
+varlist.insert(1, "donde?")
+varlist.append('usco')
+#varlist.append(input('Inserte ciudad: '))
+
+
+for z in varlist:
+    print(z)
+    
+print('----------------------------------------------------------')
+print('----------------------------------------------------------')
+
+q = 0
+while q < len(varlist):
+    print(varlist[q])
+    q += 1
+   
+print('----------------------------------------------------------')
+print('----------------------------------------------------------')
+'''
+'''
+eliminar = ('Digite el calor a eliminar: ')
+contador = 0
+for z in varlist:
+    if eliminar == z:
+        del varlist[contador]
+    else:
+        contador += 1
+        
+print(varlist)
+
+print('----------------------------------------------------------')
+print('----------------------------------------------------------')
+
+'''
+
+
+
+
+datos = [[['hector', 'duvan'], ['doz', 'aaaaaa']],[['he1ctor', 'du1van'], ['do1z', 'aaa1aaa']]]
+
+print(datos[1][1][1])
+
+print('----------------------------------------------------------')
+print('----------------------------------------------------------')
+
+for i in datos:
+    for u in i:
+        for x in u:
+            print(x)
+        print('---------')
